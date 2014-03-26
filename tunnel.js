2261a9a69c491389083e26598e6fc24954f2ba3c
IMaz6 <ihtisham-6@hotmail.com>
2014-03-26 18:13:19 -0500

    These are all the tunnel points of the campus

---
+++ tunnel.js
@@ -0,0 +1,435 @@
+//TUNNEL points starting from leeds
+//Larona
+var T0 = new Object({
+			lat: 45.388029,
+			lng: -75.698504,
+			name: "T0",
+			building: "LE"
+		});
+var T1 = new Object({
+			lat: 45.388029,
+			lng: -75.697882,
+			name: "T1",
+			building: "null"
+		});
+var T2 = new Object({
+			lat: 45.387562,
+			lng: -75.697839,
+			name: "T2",
+			building: "null"
+		});
+
+var T3 = new Object({
+			lat: 45.387389,
+			lng: -75.69785,
+			name: "T3",
+			building: "null"
+		});
+var T4 = new Object({
+			lat: 45.387381,
+			lng: -75.698043,
+			name: "T4",
+			building: "SP"
+		});
+var T5 = new Object({
+			lat: 45.387426,
+			lng: -75.697399,
+			name: "T5",
+			building: "SO"
+		});
+
+var T6 = new Object({
+			lat: 45.386816,
+			lng: -75.697829,
+			name: "T6",
+			building: "null"
+		});
+var T7 = new Object({
+			lat: 45.386816,
+			lng: -75.698376,
+			name: "T7",
+			building: "RU"
+		});
+var T8 = new Object({
+			lat: 45.386831,
+			lng: -75.697303,
+			name: "T8",
+			building: "CO"
+		});
+var T9 = new Object({
+			lat: 45.386251,
+			lng: -75.697861,
+			name: "T9",
+			building: "null"
+		});
+var T10 = new Object({
+			lat: 45.385904,
+			lng: -75.697711,
+			name: "T10",
+			building: "null"
+		});		
+var T11 = new Object({
+			lat: 45.385912,
+			lng: -75.697432,
+			name: "T11",
+			building: "null"
+		});
+var T12 = new Object({
+			lat: 45.386047,
+			lng: -75.697432,
+			name: "T12",
+			building: "RH"
+		});
+var T13 = new Object({
+			lat: 45.385912,
+			lng: -75.698225,
+			name: "T13",
+			building: "FR"
+		});		
+var T14 = new Object({
+			lat: 45.384374,
+			lng: -75.69564,
+			name: "T14",
+			building: "null"
+		});		
+var T15 = new Object({
+			lat: 45.384555,
+			lng: -75.694234,
+			name: "T15",
+			building: "null"
+		});	
+var T16 = new Object({
+			lat: 45.384367,
+			lng: -75.694116,
+			name: "T16",
+			building: "null"
+		});				
+var T17 = new Object({
+			lat: 45.38442,
+			lng: -75.693816,
+			name: "T17",
+			building: "TT"
+		});		
+var T18 = new Object({
+			lat: 45.38396,
+			lng: -75.69374,
+			name: "T18",
+			building: "null"
+		});	
+var T19 = new Object({
+			lat: 45.384066,
+			lng: -75.693355,
+			name: "T19",
+			building: "NB"
+		});
+var T20 = new Object({
+			lat: 45.383595,
+			lng: -75.694197,
+			name: "T20",
+			building: "null"
+		});	
+		
+var T21 = new Object({
+			lat: 45.383493,
+			lng: -75.694503,
+			name: "T21",
+			building: "RO1"
+		});	
+var T22 = new Object({
+			lat: 45.383417,
+			lng: -75.694674,
+			name: "T22",
+			building: "null"
+		});		
+var T23 = new Object({
+			lat: 45.383199,
+			lng: -75.694492,
+			name: "T22",
+			building: "R02"
+		});	
+var T24 = new Object({
+			lat: 45.385761,
+			lng: -75.694942,
+			name: "T24",
+			building: "null"
+		});		
+var T25 = new Object({
+			lat: 45.385889,
+			lng: -75.694642,
+			name: "T25",
+			building: "null"
+		});	
+var T26 = new Object({
+			lat: 45.385957,
+			lng: -75.694202,
+			name: "T26",
+			building: "AC"
+		});	
+var T27 = new Object({
+			lat: 45.383681,
+			lng: -75.694696,
+			name: "T20",
+			building: "uhh?"
+		});
+
+		
+		
+////Mazhar Start
+var T28 = new Object({
+			lat: 45.385098,
+			lng: -75.697216,
+			name: "T28",
+			building: "MC"
+		});
+var T29 = new Object({
+	lat: 45.385042,
+	lng: -75.697501,
+	name: "T29",
+	building: "null"
+});
+var T30 = new Object({
+	lat: 45.384959,
+	lng: -75.69779,
+	name: "T30",
+	building: "ME"
+});
+var T31 = new Object({
+	lat: 45.384435,
+	lng: -75.696664,
+	name: "T31",
+	building: "null"
+});
+var T32 = new Object({
+	lat: 45.38416,
+	lng: -75.697399,
+	name: "T32",
+	building: "null"
+});
+var T33 = new Object({
+	lat: 45.384059,
+	lng: -75.697662,
+	name: "T33",
+	building: "AA"
+});
+var T34 = new Object({
+	lat: 45.384228,
+	lng: -75.697833,
+	name: "T34",
+	building: "ME"
+});
+var T35 = new Object({
+	lat: 45.383893,
+	lng: -75.697994,
+	name: "T35",
+	building: "null"
+});
+var T36 = new Object({
+	lat: 45.383814,
+	lng: -75.698171,
+	name: "T36",
+	building: "UC"
+});
+var T37 = new Object({
+	lat:45.383158,
+	lng: -75.698944,
+	name: "T37",
+	building: "null"
+});
+var T38 = new Object({
+	lat:45.382917,
+	lng: -75.698713,
+	name: "T38",
+	building: "null"
+});
+var T39 = new Object({
+	lat:45.382736,
+	lng: -75.698885,
+	name: "T39",
+	building: "AP"
+});
+var T40 = new Object({
+	lat:45.382533,
+	lng: -75.699271,
+	name: "T40",
+	building: "DT"
+});
+var T41 = new Object({
+	lat:45.382423,
+	lng: -75.699571,
+	name: "T41",
+	building: "null"
+});
+var T42 = new Object({
+	lat:45.382016,
+	lng: -75.699126,
+	name: "T42",
+	building: "ML"
+});
+var T43 = new Object({
+	lat:45.381858,
+	lng: -75.698965,
+	name: "T43",
+	building: "null"
+});
+var T44 = new Object({
+	lat:45.38202,
+	lng: -75.698595,
+	name: "T44"
+	building: "PA"
+});
+var T45 = new Object({
+	lat:45.382224,
+	lng: -75.698032,
+	name: "T45"
+	building: "null"
+});
+var T46 = new Object({
+	lat:45.382521,
+	lng: -75.698322,
+	name: "T46"
+	building: "TB"
+});
+var T47 = new Object({
+	lat:45.382137,
+	lng: -75.698005,
+	name: "T47"
+	building: "null"
+});
+var T48 = new Object({
+	lat:45.382502,
+	lng: -75.697571,
+	name: "T48"
+	building: "null"
+});
+var T49 = new Object({
+	lat:45.382608,
+	lng: -75.697281,
+	name: "T49"
+	building: "null"
+});
+var T50 = new Object({
+	lat:45.382348,
+	lng: -75.697061,
+	name: "T50"
+	building: "HP"
+});
+var T51 = new Object({
+	lat:45.382525,
+	lng: -75.696659,
+	name: "T51"
+	building: "SC"
+});
+var T52 = new Object({
+	lat:45.383184,
+	lng: -75.697962,
+	name: "T52"
+	building: "UC"
+});
+var T53 = new Object({
+	lat:45.383083,
+	lng: -75.698391,
+	name: "T53"
+	building: "TB"
+});
+var T54 = new Object({
+	lat:45.381764,
+	lng: -75.698043,
+	name: "T54"
+	building: "null"
+});
+var T55 = new Object({
+	lat:45.381376,
+	lng: -75.698145,
+	name: "T55"
+	building: "LS"
+});
+var T56 = new Object({
+	lat:45.380999,
+	lng: -75.699142,
+	name: "T56"
+	building: "LB"
+});
+var T57 = new Object({
+	lat:45.381285,
+	lng: -75.699421,
+	name: "T57"
+	building: "SA"
+});
+var T58 = new Object({
+	lat:45.381587,
+	lng: -75.699748,
+	name: "T58"
+	building: "ML"
+});
+var T59 = new Object({
+	lat:45.381704,
+	lng: -75.699271,
+	name: "T59"
+	building: "null"
+});
+var T60 = new Object({
+	lat:45.380524,
+	lng: -75.700333,
+	name: "T60"
+	building: "VS"
+});
+T1.surr=new Array(T2);	
+T2.surr=new Array(T1,T3);	
+T3.surr=new Array(T4,T2,T5,T6);
+T4.surr=new Array(T3);
+T5.surr=new Array(T3);
+T6.surr=new Array(T3,T7,T8,T9);
+T7.surr=new Array(T6);
+T8.surr=new Array(T6);
+T9.surr=new Array(T6,T10);
+T10.surr=new Array(T11,T9,T13,T28)//insert closest point on minto side Mazhar
+T11.surr=new Array(T10,T12);
+T12.surr=new Array(T11);
+T13.surr=new Array(T10);
+T14.surr=new Array(T15,T31);//insert closest point from azrieli school of arch.
+T15.surr=new Array(T14,T16,T24);
+T16.surr=new Array(T15,T17,T18);
+T17.surr=new Array(T16);
+T18.surr=new Array(T16,T19,T20);
+T19.surr=new Array(T18);
+T20.surr=new Array(T18,T21);
+T21.surr=new Array(T20,T22,T27);
+T22.surr=new Array(T21,T23);
+T23.surr=new Array(T22);
+T24.surr=new Array(T15,T25);
+T25.surr=new Array(T24,T26);
+T26.surr=new Array(T25);
+T27.surr=new Array(T21);
+T28.surr = new Array(T29, T31);
+T29.surr = new Array(T28, T30);
+T30.surr = new Array(T29, T28);
+T31.surr = new Array(T28, T32);
+T32.surr = new Array(T31, T33);
+T33.surr = new Array(T34, T32,T35);
+T34.surr = new Array(T33);
+T35.surr = new Array(T33,T36);
+T36.surr = new Array(T35,T52,T37);
+T37.surr = new Array(T36,T38);
+T38.surr = new Array(T37,T53,T39,T46);
+T39.surr = new Array(T40,T38);
+T40.surr = new Array(T39,T41);
+T41.surr = new Array(T42,T40);
+T42.surr = new Array(T41,T43);
+T43.surr = new Array(T59,T44,T42);
+T44.surr = new Array(T43,T45);
+T45.surr = new Array(T44,T46,T47);
+T46.surr = new Array(T45,T38);
+T47.surr = new Array(T45,T48,T54);
+T48.surr = new Array(T47,T49);
+T49.surr = new Array(T48,T50);
+T50.surr = new Array(T49,T51);
+T51.surr = new Array(T50);
+T52.surr = new Array(T49,T36,T53);
+T53.surr = new Array(T38,T52);
+T54.surr = new Array(T47,T55);
+T55.surr = new Array(T54,T56);
+T56.surr = new Array(T55,T60,T57);
+T57.surr = new Array(T56,T58);
+T58.surr = new Array(T57,T59);
+T59.surr = new Array(T58,T43);
+T60.surr = new Array(T56);
